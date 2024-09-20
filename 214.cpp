400d70e9827d207f13d80755dded9c4ad285faba
Vivekdhakad04 <148355380+Vivekdhakad04@users.noreply.github.com>
2024-09-20 21:35:54 +0530

    Create 214.cpp
    Shortest Palindrome

---
+++ 214.cpp
@@ -0,0 +1,20 @@
+//T.C : O(n^2)
+//S.C : O(n)
+
+class Solution {
+public:
+    string shortestPalindrome(string s) {
+        string rev = s;
+        reverse(begin(rev), end(rev));
+
+        for(int i = 0; i < s.length(); i++) {
+
+            if(!memcmp(s.c_str(), rev.c_str() + i, s.length()-i)) { 
+                return rev.substr(0, i) + s;
+            }
+        }
+
+        return rev + s;
+        
+    }
+};
