47dcfd61814a3a67dc5de0f715b044a7bee967b6
Vivek Dhakad <148355380+Vivekdhakad04@users.noreply.github.com>
2025-04-20 23:50:51 +0530

    Time: 0 ms (100%), Space: 19.9 MB (60.93%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -2,17 +2,59 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         int n = nums.size();
-        unordered_map<int,int> mpp;
-        vector<int> result;
+    //WITHOUT USING EXTRA SPACE
+    //IN CONSTANT SPACE COMPLEXITY
+    vector<int> result;
+    int first = INT_MAX, second = INT_MAX;
+    int count1 = 0, count2 = 0;
 
-        for(auto& num : nums)
-        mpp[num]++;
+    for(int i = 0 ; i < n; i++)
+    {
+        int num = nums[i];
+        if(first == num)
+        count1++;
 
-        for(auto value : mpp)
+        else if(second == num)
+        count2++;
+
+        else if(count1 == 0)
         {
-            if(value.second > n/3)
-            result.push_back(value.first);
+            count1++;
+            first = num;
         }
-    return result;
+
+        else if(count2 == 0)
+        {
+            count2++;
+            second = num;
+        }
+
+        else{
+            count1--;
+            count2--;
+        }
+    
+    }
+    
+    count1 = 0;
+    count2 = 0;
+    for(int i = 0; i < n; i++)
+    {
+        int num = nums[i];
+        if(first == num)
+        count1++;
+
+        else if(second == num)
+        count2++;
+    }
+
+    if(count1 > n/3)
+    result.push_back(first);
+
+    if(count2 > n/3)
+    result.push_back(second);
+
+return result;
+    
     }
 };
