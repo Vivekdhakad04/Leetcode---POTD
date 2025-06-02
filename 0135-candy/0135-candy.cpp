9023a27d3bf364c174d1c38e8443ecc5d9c09618
Vivek Dhakad <148355380+Vivekdhakad04@users.noreply.github.com>
2025-06-03 00:06:10 +0530

    Time: 0 ms (100%), Space: 23.4 MB (17.97%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -1,22 +1,24 @@
 class Solution {
- public:
-  int candy(vector<int>& ratings) {
-    const int n = ratings.size();
-    int ans = 0;
-    vector<int> l(n, 1);
-    vector<int> r(n, 1);
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        vector<int> left(n,1);
+        vector<int> right(n,1);
 
-    for (int i = 1; i < n; ++i)
-      if (ratings[i] > ratings[i - 1])
-        l[i] = l[i - 1] + 1;
+        for(int i = 1; i < n; i++){
+            if(ratings[i] > ratings[i-1])
+            left[i] = left[i-1] + 1;
+        }
+        
+        for(int i = n-2; i >= 0; i--){
+            if(ratings[i] > ratings[i+1])
+            right[i] = right[i+1] + 1;
+        }
 
-    for (int i = n - 2; i >= 0; --i)
-      if (ratings[i] > ratings[i + 1])
-        r[i] = r[i + 1] + 1;
+        int total = 0;
+        for(int i = 0; i < n; i++)
+        total += max(left[i], right[i]);
 
-    for (int i = 0; i < n; ++i)
-      ans += max(l[i], r[i]);
-
-    return ans;
-  }
+    return total;
+    }
 };
